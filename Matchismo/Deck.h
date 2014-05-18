acfb5877cd7a94370faea863addf8bcab606fa0b
Sam McDavid <mcdavidsa@gmail.com>
2014-05-18 14:57:53 -0400

    Model, View, and Controller groupings created. Card and Deck classes created.

---
+++ Matchismo/Deck.h
@@ -0,0 +1,18 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/18/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+
+@interface Deck : NSObject
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+- (void)addCard:(Card *)card;
+- (Card *)drawRandomCard;
+
+@end
---
+++ Matchismo/Deck.m
@@ -0,0 +1,46 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/18/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface Deck()
+
+@property (strong, nonatomic) NSMutableArray *cards; // of Card
+
+@end
+
+@implementation Deck
+
+- (NSMutableArray *)cards {
+    if (!_cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop {
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+}
+
+- (void)addCard:(Card *)card {
+    [self addCard:card atTop:NO];
+}
+
+- (Card *)drawRandomCard {
+    Card *randomCard = nil;
+    
+    unsigned index = arc4random() % [self.cards count];
+    randomCard = self.cards[index];
+    [self.cards removeObjectAtIndex:index];
+    
+    return randomCard;
+}
+
+@end
