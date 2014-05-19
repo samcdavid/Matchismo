4f0d33d85bca069aaa2c1c508b0cac7d996e9793
Sam McDavid <mcdavidsa@gmail.com>
2014-05-18 20:46:17 -0400

    Slides completed, ready to start the assignment.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -10,6 +10,9 @@
 
 @interface CardGameViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) NSInteger flipCount;
+
 @end
 
 @implementation CardGameViewController
@@ -24,6 +27,28 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setFlipCount:(NSInteger)flipCount {
+    if (!_flipCount) {
+        _flipCount = 0;
+    }
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %ld", self.flipCount];
+    NSLog(@"flipCount changed to %ld", self.flipCount);
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"A♣" forState:UIControlStateNormal];
+    }
+    self.flipCount++;
 }
 
 @end
