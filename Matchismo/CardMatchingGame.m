e2ff7ea4c8017b6b2453974879fa46f43c46e139
Sam McDavid <mcdavidsa@gmail.com>
2014-05-20 16:46:12 -0400

    Private properties, property methods, and inits created for CardMatchingGame.

--- Matchismo/CardMatchingGame.m
+++ Matchismo/CardMatchingGame.m
@@ -10,12 +10,48 @@
 
 @interface CardMatchingGame()
 
+/**
+ *  Redeclare score so that it may be editable within the implementation of CardMatchingGame.
+ */
 @property (nonatomic, readwrite) NSInteger score;
+
+/**
+ *  Cards in use in the game.
+ */
+@property (nonatomic) NSMutableArray *cards;
 
 @end
 
 @implementation CardMatchingGame
 
+// Property Methods
+- (NSMutableArray *)_cards {
+    if (!_cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+// Public Methods
+- (instancetype)init {
+    return nil;
+}
+
+- (instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck {
+    self = [super init];
+    
+    if (self) {
+        for (int i = 0; i < count; i++) {
+            Card *card = [deck drawRandomCard];
+            if (card) {
+                [self.cards addObject:card];
+            } else {
+                self = nil;
+                break;
+            }
+        }
+    }
+    
+    return self;
+}
 
 
 @end
