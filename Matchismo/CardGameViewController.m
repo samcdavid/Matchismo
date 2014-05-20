0b780c64cbd4278bd97bf30873f3b8032a38f10b
Sam McDavid <mcdavidsa@gmail.com>
2014-05-20 16:24:51 -0400

    Changed deck getter to use an instance method called createDeck in order to make it easier to redial later on.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -44,9 +44,13 @@
 
 - (Deck *)deck {
     if (!_deck) {
-        _deck = [[PlayingCardDeck alloc] init];
+        _deck = [self createDeck];
     }
     return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
 }
 
 // Instance Methods
