344b112df84197af012e44b9a853a784ff1d9b2b
Sam McDavid <mcdavidsa@gmail.com>
2014-05-20 17:09:53 -0400

    Override match: method for PlayingCard. Add HeaderDoc comments for PlayingCard.

--- Matchismo/PlayingCard.h
+++ Matchismo/PlayingCard.h
@@ -10,10 +10,28 @@
 
 @interface PlayingCard : Card
 
+/**
+ *  Suit of the playing card.
+ */
 @property (strong, nonatomic) NSString *suit;
+
+/**
+ *  Rank of the playing card.
+ */
 @property (nonatomic) NSUInteger rank;
 
+/**
+ *  Class method to define an array of valid suits.
+ *
+ *  @return NSArray of valid suits.
+ */
 + (NSArray *)validSuits;
+
+/**
+ *  The largest rank allowed for a playing card.
+ *
+ *  @return The largest rank allowed for a playing card.
+ */
 + (NSUInteger)maxRank;
 
 @end
--- Matchismo/PlayingCard.m
+++ Matchismo/PlayingCard.m
@@ -51,4 +51,20 @@
     return [[PlayingCard rankStrings][self.rank] stringByAppendingString:self.suit];
 }
 
+- (int)match:(NSArray *)otherCards {
+    int score = 0;
+    
+    if ([otherCards count] >= 1) {
+        for (PlayingCard *otherCard in otherCards) {
+            if (otherCard.rank == self.rank) {
+                score += 4;
+            } else if ([otherCard.suit isEqualToString:self.suit]) {
+                score += 1;
+            }
+        }
+    }
+    
+    return score;
+}
+
 @end
