059f40d6437ad5f0e62979b4d42ad819bd98b98b
Sam McDavid <mcdavidsa@gmail.com>
2014-05-20 17:02:00 -0400

    chooseCardAtIndex: method written along with score constants.

--- Matchismo/CardMatchingGame.m
+++ Matchismo/CardMatchingGame.m
@@ -24,6 +24,11 @@
 
 @implementation CardMatchingGame
 
+// Constants
+static const int MATCH_BONUS = 4;
+static const int MISMATCH_PENALTY = 2;
+static const int COST_TO_CHOOSE = 1;
+
 // Property Methods
 - (NSMutableArray *)_cards {
     if (!_cards) _cards = [[NSMutableArray alloc] init];
@@ -51,6 +56,33 @@
     }
     
     return self;
+}
+
+- (void)chooseCardAtIndex:(NSUInteger)index {
+    Card *card = [self cardAtIndex:index];
+    
+    if (!card.isMatched) {
+        if (card.isChosen) {
+            card.chosen = NO;
+        } else {
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    NSInteger matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        self.score += matchScore * MATCH_BONUS;
+                        otherCard.matched = YES;
+                        otherCard.chosen = YES;
+                    } else {
+                        self.score -= MISMATCH_PENALTY;
+                        otherCard.chosen = NO;
+                    }
+                    break;
+                }
+            }
+            self.score -= COST_TO_CHOOSE;
+            card.chosen = YES;
+        }
+    }
 }
 
 - (Card *)cardAtIndex:(NSUInteger)index {
