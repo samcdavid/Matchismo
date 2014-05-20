a2f7ace1da05f84d1d7afa78a34097b2f989f858
Sam McDavid <mcdavidsa@gmail.com>
2014-05-20 16:37:47 -0400

    Create model for CardMatchingGame.

---
+++ Matchismo/CardMatchingGame.h
@@ -0,0 +1,47 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/20/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+/**
+ *  Score of the CardMatchingGame is determined privately within the implementation of CardMatchingGame.
+ */
+@property (nonatomic, readonly) NSInteger score;
+
+/**
+ *  This is the designated init method for CardMatchingGame.
+ *  The inherited init method will return nil.
+ *
+ *  @param count The number of cards to use with the card matching game.
+ *  @param deck  The deck to be used with the game.
+ *
+ *  @return A CardMatchingGame
+ */
+- (instancetype)initWithCardCount: (NSUInteger)count
+                        usingDeck: (Deck *)deck;
+
+/**
+ *  Choose a card in the game.
+ *
+ *  @param index The index of the card in the game.
+ */
+- (void)chooseCardAtIndex: (NSUInteger)index;
+
+/**
+ *  Retrieve a card in the game with a certain index.
+ *
+ *  @param index Index of the card to be retrieved.
+ *
+ *  @return Card at the given index.
+ */
+- (Card *)cardAtIndex: (NSUInteger)index;
+
+@end
---
+++ Matchismo/CardMatchingGame.m
@@ -0,0 +1,21 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/20/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@interface CardMatchingGame()
+
+@property (nonatomic, readwrite) NSInteger score;
+
+@end
+
+@implementation CardMatchingGame
+
+
+
+@end
