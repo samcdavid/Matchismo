0cd196dd337739844c94313d6e8f937bffee22cf
Sam McDavid <mcdavidsa@gmail.com>
2014-05-24 12:01:37 -0400

    Add headerdoc comments.

--- Matchismo/Card.h
+++ Matchismo/Card.h
@@ -10,11 +10,30 @@
 
 @interface Card : NSObject
 
+/**
+ *  An NSString property for storing the contents of a card.
+ */
 @property (strong, nonatomic) NSString *contents;
 
+/**
+ *  A boolean property for storing whether or not a card in the game is chosen.
+ */
 @property (nonatomic, getter = isChosen) BOOL chosen;
+
+/**
+ *  A boolean property for storing whether or not a card in the game 
+ *  has been matched to another.
+ */
 @property (nonatomic, getter = isMatched) BOOL matched;
 
+/**
+ *  Determine if a card is matched to any card in the set and calculate the 
+ *  appropriate score.
+ *
+ *  @param otherCards Set of cards to check for a match.
+ *
+ *  @return The caculated score.
+ */
 - (int)match:(NSArray *)otherCards;
 
 @end
