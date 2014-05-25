e4a5636a24ca0720651a2c77ee00bcdece838def
Sam McDavid <mcdavidsa@gmail.com>
2014-05-25 18:01:52 -0400

    Hook up SetCardGameViewController

--- Matchismo/SetCardGameViewController.m
+++ Matchismo/SetCardGameViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "SetCardGameViewController.h"
+#import "SetCardDeck.h"
+#import "MovesViewController.h"
 
 @interface SetCardGameViewController ()
 
@@ -14,36 +16,22 @@
 
 @implementation SetCardGameViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (Deck *)createDeck {
+    return [[SetCardDeck alloc] init];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"SetCardGameMovesSegue"] &&
+        [segue.destinationViewController isKindOfClass:[MovesViewController class]]) {
+        MovesViewController *turnDescriptionController = (MovesViewController *)segue.destinationViewController;
+        [turnDescriptionController setTurnDescriptions:[self attributedCardGameTurnDescriptions]];
+    }
 }
-*/
+
 
 @end
