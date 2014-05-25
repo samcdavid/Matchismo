5b3e7aa8ee11bae500a1e6aaab1894d46ed5c1c2
Sam McDavid <mcdavidsa@gmail.com>
2014-05-25 17:55:15 -0400

    Create SetCardGameViewController class.

---
+++ Matchismo/SetCardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetCardGameViewController.h
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/25/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface SetCardGameViewController : CardGameViewController
+
+@end
