676ad58b509e2ff1f74996bd2dd5b2880b47e47a
Sam McDavid <mcdavidsa@gmail.com>
2014-05-25 17:40:17 -0400

    Create SetCardDeck class.

---
+++ Matchismo/SetCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  SetCardDeck.h
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/25/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface SetCardDeck : Deck
+
+@end
---
+++ Matchismo/SetCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  SetCardDeck.m
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/25/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import "SetCardDeck.h"
+
+@implementation SetCardDeck
+
+@end
