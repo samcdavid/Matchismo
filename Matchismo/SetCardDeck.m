189605acb44590e87b1718c98af8b1fe37940a7a
Sam McDavid <mcdavidsa@gmail.com>
2014-05-25 17:49:19 -0400

    Implement SetCardDecks init and numberOfCardsToMatch.

--- Matchismo/SetCardDeck.m
+++ Matchismo/SetCardDeck.m
@@ -10,4 +10,29 @@
 
 @implementation SetCardDeck
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        for (int i = 0; i < [SetCard maxRank]; i++) {
+            for (NSString *symbol in [SetCard validSymbols]) {
+                for (NSString *shade in [SetCard validShades]) {
+                    for (NSString *color in [SetCard validColors]) {
+                        SetCard *newSetCard = [[SetCard alloc] init];
+                        newSetCard.rank = i;
+                        newSetCard.symbol = symbol;
+                        newSetCard.shadeString = shade;
+                        newSetCard.colorString = color;
+                        [self addCard:newSetCard];
+                    }
+                }
+            }
+        }
+    }
+    return self;
+}
+
+- (NSUInteger)numberOfCardsToMatch {
+    return 3;
+}
+
 @end
