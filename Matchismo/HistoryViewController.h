3a780cfd21ce755fb000a1b5e98f0b3b840f7647
Sam McDavid <mcdavidsa@gmail.com>
2014-05-26 20:19:52 -0400

    Create HistoryViewController

---
+++ Matchismo/HistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.h
+//  Matchismo
+//
+//  Created by Sam McDavid on 5/26/14.
+//  Copyright (c) 2014 Sam McDavid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryViewController : UIViewController
+
+@end
